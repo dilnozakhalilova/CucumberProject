4b47fede44bf45599dce50492103d56a7a282008
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-03-03 20:28:05 -0500

    String

--- src/test/java/steps/SearchEmployeeSteps.java
+++ src/test/java/steps/SearchEmployeeSteps.java
@@ -7,6 +7,7 @@ import pages.LoginPage;
 
 
 public class SearchEmployeeSteps extends BaseClass {
+    String expectedEmployeeId = "0909";
 
     @Given("user is on the Exelenter homepage")
     public void user_is_on_the_exelenter_homepage() {
@@ -16,20 +17,18 @@ public class SearchEmployeeSteps extends BaseClass {
 
     @Given("user logs in with valid admin credentials")
     public void user_logs_in_with_valid_admin_credentials() {
-        loginPage.loginToWebsiteViaConfigs("username","password");
+        loginPage.loginToWebsiteViaConfigs("username", "password");
 
     }
 
     @Given("user navigates to the employee list page")
     public void user_navigates_to_the_employee_list_page() {
-
-
-
+        pimPage.navigateToEmployeeList();
     }
 
     @Given("user enters an existing employee id in the id search-field")
     public void user_enters_an_existing_employee_id_in_the_id_search_field() {
-
+        employeeListPage.searchEmployeeById();
     }
 
     @When("user clicks on the search button")
