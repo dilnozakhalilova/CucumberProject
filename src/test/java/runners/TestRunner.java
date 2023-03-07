38583236e81ab8ad2305306bab716859dc44fb33
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-03-06 22:13:03 -0500

    adding path

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-        features = "src/test/resources/features/searchEmployee.feature",  // specify path from content root, features option will establish connection with feature file(s)
-        glue = "steps/SearchEmployeeSteps.java"      // specify path from source root, glue will establish connection with step definitions
+        features = "src/test/resources/features",  // specify path from content root, features option will establish connection with feature file(s)
+        glue = "steps"      // specify path from source root, glue will establish connection with step definitions
 )
 
 
