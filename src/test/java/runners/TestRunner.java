6262b0b3ba9e7ef364dd7ca0fd6c05962f72c344
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-03-07 21:38:17 -0500

    Updates

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -11,7 +11,10 @@ import org.junit.runner.RunWith;
         glue = "steps",     // specify path from source root, glue will establish connection with step definitions
         dryRun = false,     // if set to true it will check if all step definitions have implementation. Like headless(without running)
         monochrome = true,  // This will print test result in more human readable format in console
-        tags =
+        tags ="@smoke"      // run(test(s) with provided tag name  // and == &&, or ==||
+
+
+
 
 )
 
