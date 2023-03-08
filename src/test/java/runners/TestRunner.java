a4cccddbec16a0f8f31d4222a4c6325f7b21c9f6
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-03-07 20:42:09 -0500

    tags

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,17 +8,12 @@ import org.junit.runner.RunWith;
 
 @CucumberOptions(
         features = "src/test/resources/features",  // specify path from content root, features option will establish connection with feature file(s)
-        glue = "steps" ,     // specify path from source root, glue will establish connection with step definitions
+        glue = "steps",     // specify path from source root, glue will establish connection with step definitions
         dryRun = false,     // if set to true it will check if all step definitions have implementation. Like headless(without running)
         monochrome = true,  // This will print test result in more human readable format in console
-
+        tags =
 
 )
-
-
-
-
-
 
 
 public class TestRunner {
