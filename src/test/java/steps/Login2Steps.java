897d79b667a65db58fc7b24a18013ed03e71cdfd
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-03-08 22:27:43 -0500

    running a test runner

--- src/test/java/steps/Login2Steps.java
+++ src/test/java/steps/Login2Steps.java
@@ -9,7 +9,7 @@ public class Login2Steps extends BaseClass {
 
     @When("admin user logs in with valid credentials")
     public void admin_user_logs_in_with_valid_credentials() {
-        loginPage.loginToWebsiteViaConfigs("username","password");
+        loginPage.loginToWebsiteViaConfigs("username", "password");
         // 2nd way
         // sendText(loginPage.username,"username");
         // sendText(loginPage.password,ConfigsReader.getProperties("password");
@@ -19,23 +19,25 @@ public class Login2Steps extends BaseClass {
 //        loginPage.username.sendKeys("Admin");
 
     }
+
     @Then("admin user logs in successfully")
     public void admin_user_logs_in_successfully() {
         String expectedMessage = "Welcome Admin";
         String actualMessage = dashboardPage.welcome.getText();
-        Assert.assertEquals("Welcome Admin message is Displayed. Test passed",expectedMessage,actualMessage);
+        Assert.assertEquals("Welcome Admin message is Displayed. Test passed", expectedMessage, actualMessage);
     }
 
     @When("ess user logs in with valid credentials")
     public void ess_user_logs_in_with_valid_credentials() {
-        loginPage.loginToWebsiteViaConfigs("essUsername","essPassword");
+        loginPage.loginToWebsiteViaConfigs("essUsername", "essPassword");
 
     }
+
     @Then("ess user logs in successfully")
     public void ess_user_logs_in_successfully() {
-String expectedMessage= "Welcome John";
-String actualMessage=dashboardPage.welcome.getText();
-Assert.assertEquals("Welcome John message is Displayed. Test passed",expectedMessage,actualMessage);
+        String expectedMessage = "Welcome John";
+        String actualMessage = dashboardPage.welcome.getText();
+        Assert.assertEquals("Welcome John message is Displayed. Test passed", expectedMessage, actualMessage);
 
     }
 
