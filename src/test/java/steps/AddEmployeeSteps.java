a4aa4ae7339f3c43e7e325a8b45ae6246fef6384
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-03-09 23:09:38 -0500

    scenario options

---
+++ src/test/java/steps/AddEmployeeSteps.java
@@ -0,0 +1,28 @@
+package steps;
+
+import base.BaseClass;
+import io.cucumber.java.en.*;
+import org.junit.Assert;
+
+public class AddEmployeeSteps extends BaseClass {
+
+
+    @When("user inputs valid {string} and {string}")
+    public void user_inputs_valid_and(String firstName, String lastName) {
+        addEmployeePage.firstName.sendKeys(firstName);
+        addEmployeePage.lastName.sendKeys(lastName);
+    }
+
+    @When("user clicks the save button")
+    public void user_clicks_the_save_button() {
+        addEmployeePage.saveBtn.click();
+
+    }
+
+    @Then("user can see {string} name displayed")
+    public void user_can_see_name_displayed(String newEmployeeDetails) {
+        Assert.assertEquals("Test failed",newEmployeeDetails,personalDetailsPage.personalDetailsHeader.getText());
+
+    }
+
+}
---
+++ src/test/resources/features/addEmployee.feature
@@ -0,0 +1,18 @@
+@addEmployeeTest
+Feature: Adding new employees
+
+  Background:
+    * user logs in with valid admin credentials
+    * user navigates to the employee list page
+
+  Scenario Outline:
+    When user inputs valid "<firstName>" and "<lastName>"
+    * user clicks the save button
+    Then user can see "<new employee>" name displayed
+    Examples:
+
+      | firstName | lastName |
+      | Maya      | Jerriho  |
+      | Sandy     | Smith    |
+      | Katie     | Jackson  |
+      | Emely     | Suhana   |
