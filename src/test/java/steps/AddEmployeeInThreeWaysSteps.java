8d95b6c29c8ea67bf40691f779bec7efba8aa7b3
Dilnoza Khalilova <dilnoza.ubaydulloeva@gmail.com>
2023-03-12 18:26:56 -0400

    Scenario 2

--- src/test/java/steps/AddEmployeeInThreeWaysSteps.java
+++ src/test/java/steps/AddEmployeeInThreeWaysSteps.java
@@ -13,8 +13,22 @@ public class AddEmployeeInThreeWaysSteps extends BaseClass {
 
     }
     @Then("{string} is successfully added")
-    public void is_successfully_added(String fullName) {
-        Assert.assertEquals("Test Failed. Employee was not added using First and Last Name",fullName,personalDetailsPage.employeeFullName.getText());
+    public void is_successfully_added(String newEmployee) {
+        Assert.assertEquals("Test Failed. Employee was not added using First and Last Name",newEmployee,personalDetailsPage.employeeFullName.getText());
+
+    }
+    @When("user inputs valid {string} and {string} and removes id")
+    public void user_inputs_valid_and_and_removes_id(String firstName, String lastName) {
+        addEmployeePage.firstName.sendKeys(firstName);
+        addEmployeePage.lastName.sendKeys(lastName);
+        addEmployeePage.employeeId.clear();
+        addEmployeePage.saveBtn.click();
+
+    }
+
+    @Then("{string} is successfully added without userID")
+    public void isSuccessfullyAddedWithoutUserID(String newEmployee) {
+        Assert.assertEquals("Test failed. New employee was not added without ID" , newEmployee,personalDetailsPage.employeeFullName.getText());
 
     }
 }
--- target/test-classes/features/addEmployeeInThreeWays.feature
+++ target/test-classes/features/addEmployeeInThreeWays.feature
@@ -5,18 +5,18 @@ Feature: Adding employees in three different ways
     Given user logs in with valid admin credentials
     And user navigates to the AddEmployee page
     When  user inputs valid "<firstName>" and "<lastName>" and clicks save button
-    Then "<fullName>" is successfully added
+    Then "<newEmployee>" is successfully added
     Examples:
-      | firstName | lastName | fullName  |
+      | firstName | lastName | newEmployee |
       | Joe       | Biden    | Joe Biden |
 
   Scenario Outline:
     Given user logs in with valid admin credentials
     And user navigates to the AddEmployee page
     When  user inputs valid "<firstName>" and "<lastName>" and removes id
-    Then "<fullName>" is successfully added without "<userID>"
+    Then "<newEmployee>" is successfully added without userID
     Examples:
-      | firstName | lastName | fullName      | userID |
+      | firstName | lastName | newEmployee     | userID |
       | Vivian    | Joseph   | Vivian Joseph |        |
 
 
